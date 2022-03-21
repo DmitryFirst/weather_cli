5534be4c096d56823a937153901cb8119ffa99c5
Kornilov <dm.korniloff@gmail.com>
2022-03-21 12:00:58 +0200

    add all files in project

--- weather.js
+++ weather.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 import { getArgs } from './helpers/args.js'
-import { getWeather } from './services/api.service';
+import { getWeather, getIcon} from './services/api.service';
 //
-import { printHelp, printSuccess, printError } from './services/log.service.js';
-import { saveKeyValue, TOKEN_DICTIONARY } from './services/storage.service.js';
+import { printHelp, printSuccess, printError, printWeather } from './services/log.service.js';
+import { saveKeyValue, TOKEN_DICTIONARY, getKeyValue} from './services/storage.service.js';
 
 const saveToken = async (token) => {
 	if(!token.length){
@@ -18,11 +18,25 @@ const saveToken = async (token) => {
 		}
 }
 
+const saveCity = async (city) => {
+	if(!city.length){
+		printError('Не передан город');
+		return;
+	}
+	try	{
+		await saveKeyValue(TOKEN_DICTIONARY.city, city)
+		printSuccess('Город сохранен');
+	}catch (e){
+			printError(e.message)
+		}
+}
+
 const getForcast =async () => {
 
 	try {
-		const weather = await getWeather('kryvyi Rih');
-		console.log(weather)
+		const city = process.env.CITY ?? await getKeyValue(TOKEN_DICTIONARY.city)
+		const weather = await getWeather(city);//'kryvyi Rih'
+		printWeather(weather, getIcon(weather.weather[0].icon))
 		
 	} catch (e) {
 		if(e?.response?.status ==404){
@@ -39,17 +53,17 @@ const initCLI = () => {
 const args = getArgs(process.argv);
 	//console.log( args);
 	if(args.h){
-		printHelp();
+		return printHelp();
 	}
 
 	if(args.s){
-		//сохранить город
+		return saveCity(args.s);
 	}
 
 	if(args.t){
 		return saveToken( args.t);
 	}
-	getForcast()	
+	return getForcast()	
 	// вывести погоду
 };
 
